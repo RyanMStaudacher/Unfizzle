a370ed068b447c76442a49a9f773bab8cd168601
Ryan Staudacher <ryanmstaudacher@gmail.com>
2019-09-02 01:11:43 -0500

    Completed part 2 of voxel terrain generation tutorial.

--- Unfizzle/Assets/Scripts/World Generation/PolygonGenerator.cs
+++ Unfizzle/Assets/Scripts/World Generation/PolygonGenerator.cs
@@ -22,42 +22,34 @@ public class PolygonGenerator : MonoBehaviour
     private Vector2 tStone = new Vector2(0, 0);
     private Vector2 tGrass = new Vector2(0, 1);
 
+    private int squareCount;
+
+    public byte[,] blocks;
+
+    public List<Vector3> colVertices = new List<Vector3>();
+    public List<int> colTriangles = new List<int>();
+    private int colCount;
+
+    private MeshCollider col;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
+        col = GetComponent<MeshCollider>();
 
-        float x = transform.position.x;
-        float y = transform.position.y;
-        float z = transform.position.z;
-
-        newVertices.Add(new Vector3(x, y, z));
-        newVertices.Add(new Vector3(x + 1, y, z));
-        newVertices.Add(new Vector3(x + 1, y - 1, z));
-        newVertices.Add(new Vector3(x, y - 1, z));
-
-        newTriangles.Add(0);
-        newTriangles.Add(1);
-        newTriangles.Add(3);
-        newTriangles.Add(1);
-        newTriangles.Add(2);
-        newTriangles.Add(3);
-
-        newUV.Add(new Vector2(tUnit * tStone.x, tUnit * tStone.y + tUnit));
-        newUV.Add(new Vector2(tUnit * tStone.x + tUnit, tUnit * tStone.y + tUnit));
-        newUV.Add(new Vector2(tUnit * tStone.x + tUnit, tUnit * tStone.y));
-        newUV.Add(new Vector2(tUnit * tStone.x, tUnit * tStone.y));
-
-        //mesh.Clear();
-        //mesh.vertices = newVertices.ToArray();
-        //mesh.triangles = newTriangles.ToArray();
-        //mesh.uv = newUV.ToArray();
-        //mesh.Optimize();
-        //mesh.RecalculateNormals();
+        GenTerrain();
+        BuildMesh();
+        UpdateMesh();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        
+    }
+
+    private void UpdateMesh()
     {
         mesh.Clear();
         mesh.vertices = newVertices.ToArray();
@@ -65,5 +57,156 @@ public class PolygonGenerator : MonoBehaviour
         mesh.uv = newUV.ToArray();
         mesh.Optimize();
         mesh.RecalculateNormals();
+
+        squareCount = 0;
+        newVertices.Clear();
+        newTriangles.Clear();
+        newUV.Clear();
+
+        Mesh newMesh = new Mesh();
+        newMesh.vertices = colVertices.ToArray();
+        newMesh.triangles = colTriangles.ToArray();
+        col.sharedMesh = newMesh;
+
+        colVertices.Clear();
+        colTriangles.Clear();
+        colCount = 0;
+    }
+
+    private void GenSquare(int x, int y, Vector2 texture)
+    {
+        newVertices.Add(new Vector3(x, y, 0));
+        newVertices.Add(new Vector3(x + 1, y, 0));
+        newVertices.Add(new Vector3(x + 1, y - 1, 0));
+        newVertices.Add(new Vector3(x, y - 1, 0));
+
+        newTriangles.Add(squareCount * 4);
+        newTriangles.Add((squareCount * 4) + 1);
+        newTriangles.Add((squareCount * 4) + 3);
+        newTriangles.Add((squareCount * 4) + 1);
+        newTriangles.Add((squareCount * 4) + 2);
+        newTriangles.Add((squareCount * 4) + 3);
+
+        newUV.Add(new Vector2(tUnit * texture.x, tUnit * texture.y + tUnit));
+        newUV.Add(new Vector2(tUnit * texture.x + tUnit, tUnit * texture.y + tUnit));
+        newUV.Add(new Vector2(tUnit * texture.x + tUnit, tUnit * texture.y));
+        newUV.Add(new Vector2(tUnit * texture.x, tUnit * texture.y));
+
+        squareCount++;
+    }
+
+    private void GenTerrain()
+    {
+        blocks = new byte[10, 10];
+
+        for (int px = 0; px < blocks.GetLength(0); px++)
+        {
+            for (int py = 0; py < blocks.GetLength(1); py++)
+            {
+                if(py == 5)
+                {
+                    blocks[px, py] = 2;
+                }
+                else if(py < 5)
+                {
+                    blocks[px, py] = 1;
+                }
+            }
+        }
+    }
+
+    private void BuildMesh()
+    {
+        for (int px = 0; px < blocks.GetLength(0); px++)
+        {
+            for (int py = 0; py < blocks.GetLength(1); py++)
+            {
+                // If the block is not air
+                if(blocks[px,py] != 0)
+                {
+                    GenCollider(px, py);
+
+                    if (blocks[px, py] == 1)
+                    {
+                        GenSquare(px, py, tStone);
+                    }
+                    else if (blocks[px, py] == 2)
+                    {
+                        GenSquare(px, py, tGrass);
+                    }
+                }
+            }
+        }
+    }
+
+    private void GenCollider(int x, int y)
+    {
+        //Top
+        if(Block(x, y + 1) == 0)
+        {
+            colVertices.Add(new Vector3(x, y, 1));
+            colVertices.Add(new Vector3(x + 1, y, 1));
+            colVertices.Add(new Vector3(x + 1, y, 0));
+            colVertices.Add(new Vector3(x, y, 0));
+
+            ColliderTriangles();
+            colCount++;
+        }
+
+        //Bot
+        if(Block(x, y - 1) == 0)
+        {
+            colVertices.Add(new Vector3(x, y - 1, 0));
+            colVertices.Add(new Vector3(x + 1, y - 1, 0));
+            colVertices.Add(new Vector3(x + 1, y - 1, 1));
+            colVertices.Add(new Vector3(x, y - 1, 1));
+
+            ColliderTriangles();
+            colCount++;
+        }
+
+        //Left
+        if(Block(x - 1, y) == 0)
+        {
+            colVertices.Add(new Vector3(x, y - 1, 1));
+            colVertices.Add(new Vector3(x, y, 1));
+            colVertices.Add(new Vector3(x, y, 0));
+            colVertices.Add(new Vector3(x, y - 1, 0));
+
+            ColliderTriangles();
+            colCount++;
+        }
+
+        //Right
+        if(Block(x + 1, y) == 0)
+        {
+            colVertices.Add(new Vector3(x + 1, y, 1));
+            colVertices.Add(new Vector3(x + 1, y - 1, 1));
+            colVertices.Add(new Vector3(x + 1, y - 1, 0));
+            colVertices.Add(new Vector3(x + 1, y, 0));
+
+            ColliderTriangles();
+            colCount++;
+        }
+    }
+
+    private void ColliderTriangles()
+    {
+        colTriangles.Add(colCount * 4);
+        colTriangles.Add((colCount * 4) + 1);
+        colTriangles.Add((colCount * 4) + 3);
+        colTriangles.Add((colCount * 4) + 1);
+        colTriangles.Add((colCount * 4) + 2);
+        colTriangles.Add((colCount * 4) + 3);
+    }
+
+    byte Block(int x, int y)
+    {
+        if(x == -1 || x == blocks.GetLength(0) || y == -1 || y == blocks.GetLength(1))
+        {
+            return (byte)1;
+        }
+
+        return blocks[x, y];
     }
 }
